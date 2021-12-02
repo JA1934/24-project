85e45c4e73283114164d85da1a5b334b794ffb9f
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-12-02 09:15:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-let value = 0
 input.onButtonPressed(Button.A, function () {
 	
 })
+let value = 0
+radio.setGroup(2500)
 basic.forever(function () {
+    pins.setAudioPin(AnalogPin.P1)
+    value = pins.analogReadPin(AnalogPin.P0)
     if (input.buttonIsPressed(Button.A)) {
         basic.showString("Welcome Master!")
     } else {
-        value = pins.analogReadPin(AnalogPin.P0)
-        if (value >= 500) {
+        if (value >= 950) {
             pins.digitalWritePin(DigitalPin.P2, 1)
-            pins.setAudioPin(AnalogPin.P1)
-            music.ringTone(262)
+            music.ringTone(1000090)
+            music.setVolume(255)
             basic.pause(500)
+            radio.sendString("Pantry Is Being Opened by Unauthorized User")
         } else {
-            basic.showIcon(IconNames.Yes)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.digitalWritePin(DigitalPin.P1, 0)
         }
     }
 })
