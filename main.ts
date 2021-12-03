4a5170ce2155358482db768e6e5e1ead379929af
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-12-03 09:25:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 let value = 0
 radio.setGroup(2500)
 basic.forever(function () {
     pins.setAudioPin(AnalogPin.P1)
     value = pins.analogReadPin(AnalogPin.P0)
     if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
         basic.showString("Welcome Master!")
     } else {
         if (value < 950) {
